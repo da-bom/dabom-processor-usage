25df3dd0754006bfb9fdeb89ea64bd2ec71dad61
ChoiSeungeon <chygold06@gmail.com>
2026-02-16 15:43:34 +0900

    DABOM-142 refactor: 사용되지 않는 필드 삭제

--- src/main/java/com/project/global/util/RedisKeyGenerator.java
+++ src/main/java/com/project/global/util/RedisKeyGenerator.java
@@ -9,7 +9,6 @@ public class RedisKeyGenerator {
     private static final String EXAMPLE_KEY_PREFIX = "example";
     private static final String FAMILY_KEY_PREFIX = "family";
     private static final String POLICY_EVENT_DEDUP_KEY_PREFIX = "event:dedup:policy";
-    private static final String EVENT_DEDUP_KEY_PREFIX = "event:dedup";
 
     public String generateFamilyAlertsKey(Long familyId) {
         return FAMILY_KEY_PREFIX + KEY_SEPARATOR + familyId + KEY_SEPARATOR + "alerts";
