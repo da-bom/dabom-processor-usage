f55e7c7f06311a3b067b0a4369bba56a16ed594d
k0081915 <k0081915@gmail.com>
2026-02-23 16:35:34 +0900

    DABOM-222 refactor: 상수 분리

---
+++ src/main/java/com/project/domain/policy/constant/PolicyConstraintKeyConstants.java
@@ -0,0 +1,12 @@
+package com.project.domain.policy.constant;
+
+public final class PolicyConstraintKeyConstants {
+    public static final String BLOCK_ACCESS = "BLOCK:ACCESS";
+    public static final String BLOCK_APP_PREFIX = "BLOCK:APP:";
+    public static final String BLOCK_TIME_START = "BLOCK:TIME:START";
+    public static final String BLOCK_TIME_END = "BLOCK:TIME:END";
+    public static final String LIMIT_DATA_MONTHLY = "LIMIT:DATA:MONTHLY";
+    public static final String VERSION_FIELD_PREFIX = "ver:";
+
+    private PolicyConstraintKeyConstants() {}
+}
