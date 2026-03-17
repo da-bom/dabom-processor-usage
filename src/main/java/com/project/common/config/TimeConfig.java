a4a1f105617314d4783fb096b6995dcf4f474d47
k0081915 <k0081915@gmail.com>
2026-03-17 20:52:52 +0900

    DABOM-487 refactor: Configuration 어노테이션 추가

--- src/main/java/com/project/common/config/TimeConfig.java
+++ src/main/java/com/project/common/config/TimeConfig.java
@@ -4,7 +4,9 @@ import java.time.Clock;
 import java.time.ZoneId;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class TimeConfig {
     public static final ZoneId ASIA_SEOUL = ZoneId.of("Asia/Seoul");
 
